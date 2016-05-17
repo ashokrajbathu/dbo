4ff39b64a349acc6a2fdefa3c6eba0c738bc00ad
shyam-krishna <shyam.krishna@vedantu.com>
2016-05-18 00:09:14 +0530

    Files from dbotica-frontend-assistant. Changes Pending

--- assistant/controllers/patientManagementCtrl.js
+++ assistant/controllers/patientManagementCtrl.js
@@ -1,6 +1,6 @@
 angular.module('personalAssistant').controller('patientManagementCtrl', ['$scope', 'dboticaServices', '$state', '$parse', '$http', 'SweetAlert', 'doctorServices', function($scope, dboticaServices, $state, $http, $parse, doctorServices, SweetAlert) {
 
-	localStorage.setItem("currentState", "patientManagement");
+    localStorage.setItem("currentState", "patientManagement");
     angular.element("#sessionDatepicker").datepicker({
         dateFormat: "dd/mm/yy",
         autoclose: true,
@@ -707,7 +707,7 @@ angular.module('personalAssistant').controller('patientManagementCtrl', ['$scope
                 $scope.patientDataInNextDiv.name = addPatientResponse[0].firstName;
                 $scope.book.label = addPatientResponse[0].firstName;
                 $scope.book.patientId = addPatientResponse[0].id;
-                $scope.addPatientBtn=true;
+                $scope.addPatientBtn = true;
             }, function() {});
         } else {
             swal({
@@ -829,5 +829,27 @@ angular.module('personalAssistant').controller('patientManagementCtrl', ['$scope
         }, function(errorResponse) {
             console.log("book error response is---", errorResponse);
         });
+    }
+
+    $scope.checkin = function(patient) {
+        var checkinBook = {};
+        console.log("patient for checkin is----", patient);
+        checkinBook.calendarStatus = "CHECK_IN";
+        checkinBook.doctorId = patient.doctorId;
+        checkinBook.eventName = patient.eventName;
+        checkinBook.label = patient.label;
+        checkinBook.patientId = patient.patientId;
+        checkinBook.startTime = patient.startTime;
+        checkinBook.id = patient.id;
+        checkinBook.state = "ACTIVE";
+        console.log("check in book is-----", checkinBook);
+        var promise = dboticaServices.cancelAppointmentOfADateOrUpdateDoctorEvent(checkinBook);
+        promise.then(function(response) {
+            console.log("after checkin is----", response);
+        }, function(errorResponse) {
+            console.log("in checkin error response");
+        })
+
+
     }
 }]);
