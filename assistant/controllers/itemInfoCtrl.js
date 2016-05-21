21b02e55908f3c5342e2b4616370b35dded31fb7
Ravi Teja <ravitejab9@gmail.com>
2016-05-22 02:09:16 +0530

    no name swal while updating

--- assistant/controllers/itemInfoCtrl.js
+++ assistant/controllers/itemInfoCtrl.js
@@ -2,6 +2,8 @@ angular.module('personalAssistant').controller('itemInfoCtrl', ['$scope', 'dboti
     localStorage.setItem("currentState", "itemInfo");
     var itemSelected;
     $scope.batches = {};
+    var batchesInfo = [];
+    $scope.informationOfBatches = [];
     itemSelected = dboticaServices.getSelectedItem();
     console.log("selected item is----", itemSelected);
     if (itemSelected !== undefined) {
@@ -16,13 +18,46 @@ angular.module('personalAssistant').controller('itemInfoCtrl', ['$scope', 'dboti
         var batchesInfo = $.parseJSON(response.data.response);
         console.log("batches info is-----", batchesInfo);
         $scope.inventoryItem = batchesInfo.inventoryItem;
-        $scope.informationOfBatches = batchesInfo.batchInfos;
+        batchesInfo = batchesInfo.batchInfos;
+        for (var itemIndex = 0; itemIndex < batchesInfo.length; itemIndex++) {
+            var newObject = {};
+            newObject.id = batchesInfo[itemIndex].id;
+            newObject.organizationId = batchesInfo[itemIndex].organizationId;
+            newObject.batchNo = batchesInfo[itemIndex].batchNo;
+            newObject.expiryTime = batchesInfo[itemIndex].expiryTime;
+            newObject.availableStock = batchesInfo[itemIndex].units;
+            newObject.totalStock = batchesInfo[itemIndex].units;
+            if (batchesInfo[itemIndex].hasOwnProperty('consumedUnits')) {
+                newObject.consumedUnits = batchesInfo[itemIndex].consumedUnits;
+                newObject.totalStock += batchesInfo[itemIndex].consumedUnits;
+            } else {
+                newObject.consumedUnits = 0;
+            }
+            if (batchesInfo[itemIndex].hasOwnProperty('expiredUnits')) {
+                newObject.expiredUnits = batchesInfo[itemIndex].expiredUnits;
+                newObject.totalStock += batchesInfo[itemIndex].expiredUnits;
+            } else {
+                newObject.expiredUnits = 0;
+            }
+            if (batchesInfo[itemIndex].hasOwnProperty('returnedUnits')) {
+                newObject.returnedUnits = batchesInfo[itemIndex].returnedUnits;
+                newObject.totalStock += batchesInfo[itemIndex].returnedUnits;
+            } else {
+                newObject.returnedUnits = 0;
+            }
+            $scope.informationOfBatches.push(newObject);
+        }
+        console.log("batches in scope are----", $scope.informationOfBatches);
         console.log("inventory item is----", $scope.inventoryItem);
         console.log("batches information is----", batchesInfo);
     }, function(errorResponse) {
         console.log("in items info error response");
 
     });
+
+    $scope.backToItems = function() {
+        $state.go('home.inventory');
+    }
 
     $scope.updateBatch = function(item, index) {
         console.log("item selected for update is----", item);
@@ -34,21 +69,6 @@ angular.module('personalAssistant').controller('itemInfoCtrl', ['$scope', 'dboti
         requestEntity.batchId = item.id;
         requestEntity.organizationId = item.organizationId;
         requestEntity.batchState = valueInSelectBox;
-        /*requestEntity.itemId = item.itemId;
-        requestEntity.costPrice = item.costPrice;*/
-        /*switch (valueInSelectBox) {
-            case 'CONSUMED':
-                requestEntity.consumedUnits = valueInTextBox;
-                break;
-            case 'EXPIRED':
-                requestEntity.expiredUnits = valueInTextBox;
-                break;
-            case 'RETURNED':
-                requestEntity.returnedUnits = valueInTextBox;
-                break;
-        }*/
-        /*requestEntity.expiryTime = item.expiryTime;
-        requestEntity.entityState = "ACTIVE";*/
         requestEntity.units = valueInTextBox;
         requestEntity = JSON.stringify(requestEntity);
         console.log("request entity is---", requestEntity);
