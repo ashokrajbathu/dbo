d5e6fa5c5c234a9c9de437960cd3d190f303f0bc
Ravi Teja <ravitejab9@gmail.com>
2016-06-03 22:23:31 +0530

    login page changes

--- assistant/controllers/main.js
+++ assistant/controllers/main.js
@@ -41,3 +41,24 @@ angular.module('personalAssistant').directive('validNumber', function() {
         }
     };
 });
+
+angular.module('personalAssistant').directive('numbersOnly', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
