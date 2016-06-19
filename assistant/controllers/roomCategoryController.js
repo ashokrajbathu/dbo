720bd76806e92740a4b9153af5e97788e2e04010
Ravi Teja <ravitejab9@gmail.com>
2016-06-19 19:51:10 +0530

    main admin controllers

---
+++ assistant/controllers/roomCategoryController.js
@@ -0,0 +1,3 @@
+angular.module('personalAssistant').controller('roomCategoryController', ['$scope', '$log', 'dboticaServices', '$state', '$parse', '$http', 'SweetAlert', 'doctorServices', function($scope, $log, dboticaServices, $state, $http, $parse, doctorServices, SweetAlert) {
+
+}]);
