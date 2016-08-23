40871b6232d0c897720702a26d4fed12e62ad5b1
Ravi Teja <ravitejab9@gmail.com>
2016-08-23 21:46:16 +0530

    templates half completed

--- assistant/controllers/addTemplateController.js
+++ assistant/controllers/addTemplateController.js
@@ -5,35 +5,84 @@ function addTemplateController($rootScope, $scope, $log, $stateParams, dboticaSe
     var addTemplate = this;
     var selectFieldTypeString = '---Select Field Type---';
     addTemplate.fieldType = "---Select Field Type---";
-    addTemplate.fieldTypesList = [{ 'fieldName': '---Select Field Type---' }, { 'fieldName': 'TEXTBOX' }, { 'fieldName': 'CHECKBOX' }, { 'fieldName': 'DROPDOWN' }, { 'fieldName': 'TEXTAREA' }]
+    addTemplate.fieldTypesList = [{ 'fieldName': '---Select Field Type---' }, { 'fieldName': 'TEXTBOX' }, { 'fieldName': 'CHECKBOX' }, { 'fieldName': 'DROPDOWN' }, { 'fieldName': 'TEXTAREA' }, { 'fieldName': 'BUTTON' }]
     addTemplate.sectionName = '';
     addTemplate.fieldName = '';
+    addTemplate.sectionNameDiv = false;
+    addTemplate.showDropdownDiv = false;
     addTemplate.selectFieldType = selectFieldType;
     addTemplate.addField = addField;
+    addTemplate.addNewDropdownField = addNewDropdownField;
+    addTemplate.selectSectionName = selectSectionName;
     var selectedFieldTypeIs = '';
+    addTemplate.addedFieldsForDropdown = '';
+    addTemplate.sectionNamesList = [];
+    addTemplate.sectionObjectsList = [];
+    var initialSectionName = '---Select Section Name---';
+    addTemplate.sectionNameToDisplay = '---Select Section Name---';
+    var sectionNameObject = { 'sectionName': '---Select Section Name---' };
+    var newSectionObject = { 'sectionName': 'New Section' };
 
     var organizationId = localStorage.getItem('orgId');
+
+    var getTemplatesPromise = dboticaServices.getAllTemplates(organizationId, 'template', true);
+    $log.log('templates promise is-------', getTemplatesPromise);
+    getTemplatesPromise.then(function(getTemplatesSuccess) {
+        var errorCode = getTemplatesSuccess.data.errorCode;
+        if (errorCode) {
+            dboticaServices.logoutFromThePage(errorCode);
+        } else {
+            var getTemplatesResponse = angular.fromJson(getTemplatesSuccess.data.response);
+            $log.log('templates Response is--------', getTemplatesResponse);
+            if (errorCode == null && getTemplatesSuccess.data.success) {
+                angular.forEach(getTemplatesResponse, function(templateEntity) {
+                    addTemplate.sectionObjectsList.push(templateEntity.templateFields);
+                });
+                angular.forEach(addTemplate.sectionObjectsList, function(objectEntity) {
+                    addTemplate.sectionNamesList.push(objectEntity[0]);
+                });
+                addTemplate.sectionNamesList = _.uniqBy(addTemplate.sectionNamesList, 'sectionName');
+                addTemplate.sectionNamesList.unshift(sectionNameObject);
+                addTemplate.sectionNamesList.push(newSectionObject);
+                $log.log('section names are-----', addTemplate.sectionNamesList);
+            }
+        }
+    }, function(getTemplatesError) {
+        dboticaServices.noConnectivityError();
+    });
 
     function selectFieldType(fieldTypeEntity) {
         addTemplate.fieldType = fieldTypeEntity.fieldName;
         switch (addTemplate.fieldType) {
             case 'TEXTBOX':
                 selectedFieldTypeIs = 'TEXT_BOX';
+                addTemplate.showDropdownDiv = false;
                 break;
             case 'CHECKBOX':
                 selectedFieldTypeIs = 'CHECK_BOX';
+                addTemplate.showDropdownDiv = true;
                 break;
             case 'DROPDOWN':
                 selectedFieldTypeIs = 'DROPDOWN';
+                addTemplate.showDropdownDiv = true;
                 break;
             case 'TEXTAREA':
                 selectedFieldTypeIs = 'TEXT_AREA';
+                addTemplate.showDropdownDiv = false;
+                break;
+            case 'BUTTON':
+                selectedFieldTypeIs = 'BUTTON';
+                addTemplate.showDropdownDiv = false;
+                break;
+            case '---Select Field Type---':
+                addTemplate.showDropdownDiv = false;
                 break;
         }
     }
 
     function addField() {
-        if (addTemplate.sectionName !== '' && addTemplate.fieldName !== '' && addTemplate.fieldType !== selectFieldTypeString) {
+        $log.log('kjsdkjs----', addTemplate.sectionName);
+        if (addTemplate.sectionName !== '' && addTemplate.sectionName !== initialSectionName && addTemplate.fieldName !== '' && addTemplate.fieldType !== selectFieldTypeString) {
             var addFieldRequestEntity = {};
             addFieldRequestEntity.organizationId = organizationId;
             addFieldRequestEntity.name = 'template';
@@ -44,13 +93,37 @@ function addTemplateController($rootScope, $scope, $log, $stateParams, dboticaSe
             templateFieldObject.fieldType = selectedFieldTypeIs;
             templateFieldObject.fieldState = 'ACTIVE';
             templateFieldObject.mandatory = true;
-            if (selectedFieldTypeIs == 'TEXT_BOX') {
+            templateFieldObject.minValue = '';
+            templateFieldObject.maxValue = '';
+            if (selectedFieldTypeIs == 'TEXT_BOX' || selectedFieldTypeIs == 'BUTTON' || selectedFieldTypeIs == 'TEXT_AREA') {
                 templateFieldObject.restrictValues = [];
-                templateFieldObject.minValue = '';
-                templateFieldObject.maxValue = '';
+                templateFieldObject.description = '';
+            }
+            if (selectedFieldTypeIs == 'DROPDOWN') {
+                var dropdownFields = [];
+                templateFieldObject.restrictValues = [];
+                dropdownFields = _.split(addTemplate.addedFieldsForDropdown, ',');
+                templateFieldObject.description = dropdownFields[0];
+                angular.forEach(dropdownFields, function(dropdownEntity) {
+                    var localFieldObject = {};
+                    localFieldObject.name = dropdownEntity;
+                    localFieldObject.value = dropdownEntity;
+                    templateFieldObject.restrictValues.push(localFieldObject);
+                });
+            }
+            if (selectedFieldTypeIs == 'CHECK_BOX') {
+                var checkboxFields = [];
+                templateFieldObject.restrictValues = [];
+                templateFieldObject.description = '';
+                checkBoxFields = _.split(addTemplate.addedFieldsForDropdown, ',');
+                angular.forEach(checkBoxFields, function(checkBoxEntity) {
+                    var localCheckObject = {};
+                    localCheckObject.name = checkBoxEntity;
+                    localCheckObject.checkBoxValue = false;
+                    templateFieldObject.restrictValues.push(localCheckObject);
+                });
             }
             templateFieldObject.name = addTemplate.fieldName;
-            templateFieldObject.description = dboticaServices.getDescription(addTemplate.fieldName);
             templateFieldObject.sectionName = addTemplate.sectionName;
             addFieldRequestEntity.templateFields.push(templateFieldObject);
             $log.log('req is----', addFieldRequestEntity);
@@ -69,6 +142,27 @@ function addTemplateController($rootScope, $scope, $log, $stateParams, dboticaSe
             });
         } else {
             dboticaServices.templateMandatoryFieldsSwal();
+        }
+    }
+
+    function addNewDropdownField() {
+        if (addTemplate.addedFieldsForDropdown == '') {
+            addTemplate.addedFieldsForDropdown += addTemplate.newDropdownFieldText;
+            addTemplate.newDropdownFieldText = '';
+        } else {
+            addTemplate.addedFieldsForDropdown += ',';
+            addTemplate.addedFieldsForDropdown += addTemplate.newDropdownFieldText;
+            addTemplate.newDropdownFieldText = '';
+        }
+    }
+
+    function selectSectionName(sectionEntity) {
+        addTemplate.sectionNameToDisplay = sectionEntity.sectionName;
+        if (sectionEntity.sectionName == 'New Section') {
+            addTemplate.sectionNameDiv = true;
+        } else {
+            addTemplate.sectionName = sectionEntity.sectionName;
+            addTemplate.sectionNameDiv = false;
         }
     }
 }
--- assistant/controllers/patientDetailsController.js
+++ assistant/controllers/patientDetailsController.js
@@ -5,12 +5,16 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
     var detail = this;
 
     detail.submitDetails = submitDetails;
+    detail.selectCheckBox = selectCheckBox;
+    detail.getData = getData;
     var organizationId = localStorage.getItem('orgId');
 
     detail.formsList = [];
     var activeTemplatesList = [];
     var localActiveTemplatesList = [];
     var localSortedTemplatesList = [];
+    var localSortedObjects = [];
+    detail.patient = {};
 
     var getTemplatesPromise = dboticaServices.getAllTemplates(organizationId, 'template', true);
     $log.log('get templates promise-----', getTemplatesPromise);
@@ -24,7 +28,6 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
             activeTemplatesList = _.filter(getTemplatesResponse, function(templateEntity) {
                 return templateEntity.state == 'ACTIVE' && templateEntity.visibility == 'ACTIVE';
             });
-            $log.log('active templates list is----', activeTemplatesList);
             angular.copy(activeTemplatesList, localActiveTemplatesList);
             angular.forEach(localActiveTemplatesList, function(activeTemplateEntity, key, value) {
                 var localSortedArray = [];
@@ -32,10 +35,28 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
                 angular.forEach(activeTemplatesList, function(localActiveTemplateEntity) {
                     if (activeTemplateEntity.templateFields[0].sectionName == localActiveTemplateEntity.templateFields[0].sectionName) {
                         localSortedArray.push(localActiveTemplateEntity.templateFields[0]);
+                        localSortedObjects.push(localActiveTemplateEntity);
                     }
                 });
-                localActiveTemplatesList.splice(key, 1);
+                angular.forEach(localSortedObjects, function(sortedEntity) {
+                    angular.forEach(localActiveTemplatesList, function(templateEntity, localKey) {
+                        if (sortedEntity.id == templateEntity.id) {
+                            localActiveTemplatesList.splice(localKey, 1);
+                        }
+                    });
+                });
                 localSortedTemplatesList.push(localSortedArray);
+            });
+            angular.forEach(localSortedTemplatesList, function(sortTempEntity) {
+                var buttonIndex = _.findLastIndex(sortTempEntity, function(buttonCheckEntity) {
+                    return buttonCheckEntity.fieldType == 'BUTTON';
+                });
+                if (buttonIndex !== undefined && buttonIndex !== -1) {
+                    var templateButtonObject = {};
+                    angular.copy(sortTempEntity[buttonIndex], templateButtonObject);
+                    sortTempEntity.splice(buttonIndex, 1);
+                    sortTempEntity.push(templateButtonObject);
+                }
             });
             angular.copy(localSortedTemplatesList, detail.formsList);
             $log.log('after sorting array is-----', localSortedTemplatesList);
@@ -44,6 +65,78 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
     }, function(getTemplatesError) {
         dboticaServices.noConnectivityError();
     });
+
+    function getData() {
+        detail.patient = dboticaServices.getPatientDetailsFromService();
+        return true;
+    }
+
+    function submitDetails(submitEntity) {
+        $log.log('in submit details');
+        $log.log('first name is---', submitEntity);
+        $log.log('inpatient is----', detail.patient);
+        $log.log('patient is-----', detail.patient);
+        var patientDetailsRequestEntity = {};
+        if (!jQuery.isEmptyObject(detail.patient)) {
+            patientDetailsRequestEntity.organizationId = organizationId;
+            patientDetailsRequestEntity.patientId = detail.patient.id;
+            patientDetailsRequestEntity.patientName = detail.patient.details.patientName;
+            patientDetailsRequestEntity.patientPhoneNumber = detail.patient.phoneNumber;
+            patientDetailsRequestEntity.patientEventType = 'PATIENT_DETAILS';
+            var currentDate = new Date();
+            patientDetailsRequestEntity.startTime = currentDate.getTime();
+            patientDetailsRequestEntity.alertTime = '';
+            patientDetailsRequestEntity.referenceId = '';
+            var patientDetails = {};
+            angular.forEach(submitEntity, function(entity) {
+                var localEntity = {};
+                angular.copy(entity, localEntity);
+                localEntity.name = dboticaServices.getDescription(localEntity.name);
+                switch (localEntity.fieldType) {
+                    case 'TEXT_BOX':
+                    case 'DROPDOWN':
+                    case 'TEXT_AREA':
+                        patientDetails[localEntity.name] = localEntity.description;
+                        break;
+                    case 'CHECK_BOX':
+                        var checkArray = [];
+                        angular.forEach(entity.restrictValues, function(restrictIs) {
+                            if (restrictIs.checkBoxValue) {
+                                checkArray.push(restrictIs.name);
+                            }
+                        });
+                        patientDetails[localEntity.name] = _.join(checkArray, ',');
+                        break;
+                    case 'BUTTON':
+                        break;
+                }
+            });
+            $log.log('patient details are-----', patientDetails);
+            patientDetails = JSON.stringify(patientDetails);
+            patientDetailsRequestEntity.referenceDetails = patientDetails;
+            $log.log('request entity is---------', patientDetailsRequestEntity);
+            var patientDetailsPromise = dboticaServices.patientEvent(patientDetailsRequestEntity);
+            $log.log('patient details promise----', patientDetailsPromise);
+            patientDetailsPromise.then(function(patientDetailsSuccess) {
+                var errorCode = patientDetailsSuccess.data.errorCode;
+                if (errorCode) {
+                    dboticaServices.logoutFromThePage(errorCode);
+                } else {
+                    var patientDetailsResponse = angular.fromJson(patientDetailsSuccess.data.response);
+                    $log.log('patient response is-----', patientDetailsResponse);
+                }
+            }, function(patientDetailsError) {
+                dboticaServices.noConnectivityError();
+            });
+        }
+    }
+
+    function selectCheckBox(checkBoxEntity) {
+        checkBoxEntity.checkBoxValue = !checkBoxEntity.checkBoxValue;
+        $log.log('in check box check----', checkBoxEntity.checkBoxValue);
+    }
+
+
 
     /*detail.formsList = [{
         formName: [{
@@ -138,10 +231,5 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
             }
         });
     });*/
-
-    function submitDetails() {
-        $log.log('in submit details');
-        $log.log('first name is---', detail.formsList);
-    }
 
 };
