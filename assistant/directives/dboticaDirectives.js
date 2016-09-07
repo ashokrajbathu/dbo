5dec3d8e97ad9d59922d0c7a8319fd8125d4056b
Ravi Teja <ravitejab9@gmail.com>
2016-09-07 10:09:22 +0530

    directive error resolved

--- assistant/directives/dboticaDirectives.js
+++ assistant/directives/dboticaDirectives.js
@@ -46,12 +46,12 @@ angular.module('personalAssistant').directive('validNumber', function() {
         };
     }).directive('numbersOnly', function() {
         return {
+            restrict: 'A',
             require: 'ngModel',
             link: function(scope, element, attr, ngModelCtrl) {
                 function fromUser(text) {
                     if (text) {
                         var transformedInput = text.replace(/[^0-9]/g, '');
-
                         if (transformedInput !== text) {
                             ngModelCtrl.$setViewValue(transformedInput);
                             ngModelCtrl.$render();
