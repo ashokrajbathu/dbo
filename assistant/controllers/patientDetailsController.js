756decd3fa863db21827a95ff52766961273f5be
Ravi Teja <ravitejab9@gmail.com>
2016-09-13 17:45:58 +0530

    template instance clearance is done

--- assistant/controllers/patientDetailsController.js
+++ assistant/controllers/patientDetailsController.js
@@ -7,15 +7,20 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
     var templatesList = [];
     detail.activeTemplate = {};
     detail.patient = {};
-    var onLoadTemplateFileds = {};
+    var onLoadTemplateFields = {};
 
     detail.submitPatientFullForm = submitPatientFullForm;
     detail.getData = getData;
 
+    /* isPalindrome('madam');
+     isPalindrome('ravi teja');*/
+    charCount('hello world');
+    swapNumber(43, 12);
+
     var assistantObject = localStorage.getItem('assistant');
     var organizationId = localStorage.getItem('orgId');
     assistantObject = angular.fromJson(assistantObject);
-    angular.copy($scope.activeTemplateFields, onLoadTemplateFileds);
+    angular.copy($scope.activeTemplateFields, onLoadTemplateFields);
 
     var getTemplatesPromise = dboticaServices.getAllTemplates(organizationId, '', true);
     getTemplatesPromise.then(function(getTemplateSuccess) {
@@ -58,19 +63,117 @@ function patientDetailsController($rootScope, $scope, $log, $stateParams, dbotic
                 var addTemplateResponse = angular.fromJson(addInstanceSuccess.data.response);
                 if (errorCode == null && addInstanceSuccess.data.success) {
                     dboticaServices.templateInstanceSuccessSwal();
-                    angular.copy(onLoadTemplateFileds, $scope.activeTemplateFields);
+                    angular.copy(onLoadTemplateFields, $scope.activeTemplateFields);
                 }
             }
         }, function(addTemplateError) {
             dboticaServices.noConnectivityError();
         });
-
     }
 
     function getData() {
         detail.patient = dboticaServices.getPatientDetailsFromService();
         return true;
     }
+
+    function charCount(string) {
+        string = string.replace(/\s/g, '');
+        string = string.toLowerCase();
+        var stringArray = string.split('');
+        var output = [];
+        var exists = {};
+        for (var index = 0; index < stringArray.length; index++) {
+            if (exists[stringArray[index]]) {
+                exists[stringArray[index]]++;
+            } else {
+                exists[stringArray[index]] = 1;
+            }
+        }
+        for (var j in exists) {
+            $log.log(j, exists[j]);
+        }
+    }
+
+    /* function charCount(string) {
+         string = string.replace(/\s/g, '');
+         string = string.toLowerCase();
+         var arr = new Array(26).fill(0);
+         for (var index = 0; index < string.length; index++) {
+             var charCode = string.charCodeAt(index);
+             arr[charCode - 97]++;
+         }
+         for (var lindex = 0; lindex < arr.length; lindex++) {
+             if (arr[lindex] !== 0) {
+                 console.log(String.fromCharCode(lindex + 97), arr[lindex]);
+             }
+         }
+     }
+
+     function removeDuplicates(string) {
+         var stringArray = string.split('');
+         var exists = {};
+         var output = [];
+         for (var index = 0; index < stringArray.length; index++) {
+             if (!exists[stringArray[index]]) {
+                 output.push(stringArray[index]);
+                 exists[stringArray[index]] = true;
+             }
+         }
+         var outputStr = output.join('');
+         $log.log('str is---', outputStr);
+     }
+
+     function reverseString(string) {
+         var stringArray = string.split('');
+         var output = [];
+         for (var index = stringArray.length - 1; index >= 0; index--) {
+             output.push(stringArray[index]);
+         }
+         var outputStr = output.join('');
+         $log.log('result is----', outputStr);
+     }
+
+     function removeDuplicateChar(string) {
+         var stringArray = string.split('');
+         var exists = {};
+         var output = [];
+         for (var index = 0; index < stringArray.length; index++) {
+             if (exists[stringArray[index]]) {
+                 exists[stringArray[index]]++;
+             } else {
+                 exists[stringArray[index]] = 1;
+             }
+         }
+         for (var j in exists) {
+             if (exists[j] == 1) {
+                 output.push(j);
+             }
+         }
+         var outputStr = output.join('');
+         $log.log('str result is----', outputStr);
+     }
+
+     function isPalindrome(string) {
+         var stringArray = string.split('');
+         var output = [];
+         for (var index = stringArray.length - 1; index >= 0; index--) {
+             output.push(stringArray[index]);
+         }
+         var outputStr = output.join('');
+         if (string == outputStr) {
+             return true;
+         } else {
+             return false;
+         }
+     }*/
+
+    /*function swapNumber(a, b) {
+        $log.log('a:' + a + "and b:" + b);
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        $log.log('after a:' + a + "and after b:" + b);
+    }*/
 
 
 };
